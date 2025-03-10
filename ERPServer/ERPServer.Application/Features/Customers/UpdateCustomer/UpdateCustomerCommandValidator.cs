90f9a55bd18b4c2001649a2f8995b43b382d601a
taberkkaya <taberkkaya@gmail.com>
2025-03-10 17:57:56 +0300

    update customer cqrs

---
+++ ERPServer/ERPServer.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ERPServer.Application.Features.Customers.UpdateCustomer;
+
+public sealed class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+{
+    public UpdateCustomerCommandValidator()
+    {
+        RuleFor(p => p.TaxNumber)
+            .MinimumLength(10)
+            .MaximumLength(11);
+
+        RuleFor(p => p.Name)
+            .MinimumLength(3);
+    }
+}
