4ee77f449f1d375cbab1a26c1a51cd1fc4aa2ff8
taberkkaya <taberkkaya@gmail.com>
2025-03-10 06:17:48 +0300

    customer repository

---
+++ ERPServer/ERPServer.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repository;
+using ERPServer.Infrastructure.Context;
+using GenericRepository;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class CustomerRepository : Repository<Customer, ApplicationDbContext>, ICustomerRepository
+{
+    public CustomerRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
