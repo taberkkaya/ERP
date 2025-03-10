264d094525fd4509a9f794ba490d8780986f69b6
taberkkaya <taberkkaya@gmail.com>
2025-03-10 17:58:56 +0300

    added mapping profiles

--- ERPServer/ERPServer.Application/Mapping/MappingProfile.cs
+++ ERPServer/ERPServer.Application/Mapping/MappingProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using ERPServer.Application.Features.Customers.CreateCustomer;
+using ERPServer.Application.Features.Customers.UpdateCustomer;
+using ERPServer.Application.Features.Depots.CreateDepot;
+using ERPServer.Application.Features.Depots.UpdateDepot;
 using ERPServer.Domain.Entities;
 
 namespace ERPServer.Application.Mapping
@@ -9,6 +12,10 @@ namespace ERPServer.Application.Mapping
         public MappingProfile()
         {
             CreateMap<CreateCustomerCommand,Customer>();
+            CreateMap<UpdateCustomerCommand,Customer>();           
+            
+            CreateMap<CreateDepotCommand,Depot>();
+            CreateMap<UpdateDepotCommand,Depot>();
         }
     }
 }
