9a892491427b217904e8f101386b6d5284e28337
taberkkaya <taberkkaya@gmail.com>
2025-03-10 07:02:27 +0300

    refactoring

--- ERPClient/src/app/services/http.service.ts
+++ ERPClient/src/app/services/http.service.ts
@@ -24,7 +24,7 @@ export class HttpService {
     this.http
       .post<ResultModel<T>>(`${api}/${apiUrl}`, body, {
         headers: {
-          Authorization: 'Bearer' + this.auth.token,
+          Authorization: 'Bearer ' + this.auth.token,
         },
       })
       .subscribe({
