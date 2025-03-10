3e18d79283a77867b4ae0045095c32a25b7afe19
taberkkaya <taberkkaya@gmail.com>
2025-03-10 17:58:06 +0300

    create depot cqrs

---
+++ ERPServer/ERPServer.Application/Features/Depots/CreateDepot/CreateDepotCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Depots.CreateDepot;
+
+public sealed record CreateDepotCommand(
+    string Name,
+    string City,
+    string Town,
+    string Address
+    ): IRequest<Result<string>>;
