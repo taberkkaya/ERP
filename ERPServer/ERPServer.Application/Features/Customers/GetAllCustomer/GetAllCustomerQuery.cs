dbbcc844ad66e34d7bc8ba289238e569662f3d9b
taberkkaya <taberkkaya@gmail.com>
2025-03-10 07:03:07 +0300

    get all customer cqrs

---
+++ ERPServer/ERPServer.Application/Features/Customers/GetAllCustomer/GetAllCustomerQuery.cs
@@ -0,0 +1,8 @@
+﻿using ERPServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Customers.GetAllCustomer;
+
+public sealed record GetAllCustomerQuery() : IRequest<
+    Result<List<Customer>>>;
