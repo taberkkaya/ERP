41af4353bf34d57df2fa4a40771ac96502ee4bff
taberkkaya <taberkkaya@gmail.com>
2025-03-10 18:41:01 +0300

    created products endpoints

---
+++ ERPServer/ERPServer.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using ERPServer.Application.Features.Products.CreateProduct;
+using ERPServer.Application.Features.Products.DeleteProductById;
+using ERPServer.Application.Features.Products.GetAllProducts;
+using ERPServer.Application.Features.Products.UpdateProduct;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controllers
+{
+    public sealed class ProductsController : ApiController
+    {
+        public ProductsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAll(GetAllProductsQuery request, CancellationToken cancellationToken) {
+            var response = await _mediator.Send(request);
+            return StatusCode(response.StatusCode,response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request);
+            return StatusCode(response.StatusCode, response);
+        }  
+        
+        [HttpPost]
+        public async Task<IActionResult> DeleteById(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request);
+            return StatusCode(response.StatusCode, response);
+        }        
+        
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
