326c3ffdd7bcf240a21fba5df2542fb97d5a22c2
taberkkaya <taberkkaya@gmail.com>
2025-03-10 18:39:50 +0300

    product type smartEnum

---
+++ ERPServer/ERPServer.Domain/Enums/ProductTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.SmartEnum;
+
+namespace ERPServer.Domain.Enums;
+
+public sealed class ProductTypeEnum : SmartEnum<ProductTypeEnum>
+{
+    public static readonly ProductTypeEnum Mamul = new("Mamul", 1);
+    public static readonly ProductTypeEnum YariMamul = new("YariMamul", 2);
+
+    public ProductTypeEnum(string name, int value) : base(name, value)
+    {
+    }
+}
