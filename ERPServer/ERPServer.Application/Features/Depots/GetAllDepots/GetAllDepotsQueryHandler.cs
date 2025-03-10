4c9f1a3abf2fa059c993184a500ee4f6e5f9e16d
taberkkaya <taberkkaya@gmail.com>
2025-03-10 17:58:28 +0300

    get all depots cqrs

---
+++ ERPServer/ERPServer.Application/Features/Depots/GetAllDepots/GetAllDepotsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Depots.GetAllDepots;
+
+internal sealed class GetAllDepotsQueryHandler(
+    IDepotRepository depotRepository
+    ) : IRequestHandler<GetAllDepotsQuery, Result<List<Depot>>>
+{
+    public async Task<Result<List<Depot>>> Handle(GetAllDepotsQuery request, CancellationToken cancellationToken)
+    {
+        List<Depot> depots = await depotRepository.GetAll().OrderBy(p => p.Name).ToListAsync(cancellationToken);
+
+        return depots;
+    }
+}
