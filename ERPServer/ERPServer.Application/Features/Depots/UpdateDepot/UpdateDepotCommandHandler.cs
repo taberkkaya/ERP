e30c082664e8ec700a3b9f249285d9a5bf9de896
taberkkaya <taberkkaya@gmail.com>
2025-03-10 17:58:37 +0300

    update depots cqrs

---
+++ ERPServer/ERPServer.Application/Features/Depots/UpdateDepot/UpdateDepotCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repository;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Depots.UpdateDepot;
+
+internal sealed class UpdateDepotCommandHandler(
+    IDepotRepository depotRepository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper
+    ) : IRequestHandler<UpdateDepotCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UpdateDepotCommand request, CancellationToken cancellationToken)
+    {
+        Depot? depot = await depotRepository.GetByExpressionWithTrackingAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (depot is null)
+            return Result<string>.Failure("Depo bulunamadı!");
+
+        bool isNameExists = await depotRepository.AnyAsync(p => p.Name.ToLower() == request.Name.ToLower(), cancellationToken);
+        if (isNameExists)
+            return Result<string>.Failure("Bu isimde bir depo zaten mevcut!");
+
+        mapper.Map(request,depot);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Depo bilgileri başarıyla güncellendi!";
+    }
+}
