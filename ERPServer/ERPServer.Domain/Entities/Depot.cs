096409cab5827cd277f6d50189713a3a8bff6c52
taberkkaya <taberkkaya@gmail.com>
2025-03-10 17:59:10 +0300

    created depot entity

---
+++ ERPServer/ERPServer.Domain/Entities/Depot.cs
@@ -0,0 +1,11 @@
+﻿using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Depot : Entity
+{
+    public string Name { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Town { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
